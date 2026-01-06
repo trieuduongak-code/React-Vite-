0de3cab4252bfcd52d9f599b63c0ed526bc2e334
NTTD <trieuduongak@gmail.com>
2026-01-06 20:50:06 +0700

    video 33

--- src/components/Learn/Mycomponent.jsx
+++ src/components/Learn/Mycomponent.jsx
@@ -1,9 +1,13 @@
 //fragment
 import "./style.css";
 const MyComponent = () => {
+  const Ychenko = {
+    name: "NTTD",
+    age: 25,
+  };
   return (
     <>
-      <div className="">NTTD 24082006</div>
+      <div className="">{JSON.stringify(Ychenko)} & 24082006</div>
       <div className="child">Child</div>
     </>
   );
